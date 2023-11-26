0db19a510de02bfcd5f6bd9837576b1fceedbe72
Saravanan K <53828581+saravanankish@users.noreply.github.com>
2023-11-26 15:39:00 +0530

    Updated content and design

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,17 @@
+const copyButtonLabel = "Copy code";
+
+async function copyCode(block, button) {
+  let code = block.querySelector("code");
+  let text = code.innerText;
+
+  await navigator.clipboard.writeText(text);
+
+  button.innerText = "Code Copied!";
+  setTimeout(() => {
+    button.innerText = copyButtonLabel;
+  }, 1000)
+}
+
 jQuery(document).ready(function ($) {
   var mastheadheight = $(".ds-header").outerHeight();
   //console.log(mastheadheight);
@@ -12,4 +26,31 @@ jQuery(document).ready(function ($) {
       }
     })
     .scroll();
+
+  $('#read-more-button').click(() => {
+    if ($('#read-more-button')[0].classList?.contains('collapsed')) {
+      window.location.replace('#aboutMe')
+    }
+  })
+
+  const pre = [...$('.ds-work-content-sec pre')]
+
+  pre.forEach((block) => {
+    // only add button if browser supports Clipboard AP
+    console.log(navigator)
+    if (navigator.clipboard) {
+      let button = document.createElement("button");
+
+      button.classList.add('ds-button')
+      button.classList.add('bs-button')
+
+      button.innerText = copyButtonLabel;
+      block.appendChild(button);
+
+      button.addEventListener("click", async () => {
+        await copyCode(block, button);
+      });
+    }
+  });
+
 });
